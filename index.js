39904a038864eaedc7df4bc1e12f9ac68dcd90f9
marsovernst-chop <marsov.ernst@gmail.com>
2021-05-07 15:27:55 +0600

    я сделал дз 1

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1- задание.
+const name = prompt('Введите имя: ');
+const surname = prompt("Введите фамилю: ");
+alert('Привет ' + name + " " + surname)
+
+// 3- задание. 
+// const color = prompt('Введите цвет:')
+
+// switch(color){
+//     case "красный":
+//         console.log("Стоять!");
+//         break;
+//     case "зеленный":
+//         console.log("Можно идти!");
+//         break;
+//     case "желтый":
+//         console.log("внимание!");
+//         break;    
+// }
+
+// 2- задаание.
+// const age = prompt('какое число больше? 9 или 6?.');
+
+// switch(age){
+//     case "9":
+//     alert("правиильно!");
+//     break;
+//     case "6":
+//     alert("не правильно!");
+//     break;    
+// }
+
+
