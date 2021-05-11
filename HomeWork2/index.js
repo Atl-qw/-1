cfa83233de826e0dfa159aded3b6ced860480ee8
Atl-qw <83763916+Atl-qw@users.noreply.github.com>
2021-05-11 20:25:35 +0600

    Add files via upload

--- HomeWork2/index.js
+++ HomeWork2/index.js
@@ -1,3 +1,27 @@
-for(let i = 0; i < 100; i++){
-    console.log(i)
-}
+// function fizzBuzz (n=100){
+//     for (let i=1; i <= n; i++) {
+//         if ((i%3==0) && (i%5==0))
+//         console.log (`${i} FizzBuzz`);
+//         else 
+//             console.log(`${i} ${!(i%3)?'Fizz':(!(i%5)?'buzz':'')}`);
+//     }
+// }
+
+// fizzBuzz();
+
+// for(var s=''; s.length < 7;) 
+//     console.log(s += '#');
+
+// let chees_board =(width,height)=>{
+//     let board = "";
+//     for(let i = 0; i< height; i++) {
+//         if(i % 2 == 0) {
+//             board += '# '.repeat(Math.round(width/2))+'\n';
+//         }else{
+//             board += ' #'.repeat(Math.round(width/2))+'\n';
+//         }
+//     }
+    
+//     console.log(board);
+// };
+// chees_board (8,8)
